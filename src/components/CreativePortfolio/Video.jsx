fabf7119fffe88e604bdc40418cb5cfd0b68a89e
Truong Nguyen <38267209+huutruong2304@users.noreply.github.com>
2023-12-06 21:16:47 +0700

    fix: change video link on mobile and display full width for project

--- src/components/CreativePortfolio/Video.jsx
+++ src/components/CreativePortfolio/Video.jsx
@@ -1,22 +1,44 @@
-"use client";
+'use client';
 
-import { useState,useRef } from "react";
+import { useState, useRef, useEffect } from 'react';
 
+const pcLink = '/assets/vid/headline.mp4';
+const mobileLink =
+  'https://res.cloudinary.com/dwvg8msdh/video/upload/v1701767766/Data%20for%20web/Video/FLOW_SHOWREELS_VERTICAL_gdzqon.mp4';
 // components/Video.js
 const Video = () => {
   const [muted, setMuted] = useState(true);
   const videoRef = useRef(null);
+  const sourceRef = useRef(pcLink);
+
+  useEffect(() => {
+    updateVideoLink();
+    window.addEventListener('resize', updateVideoLink);
+
+    return () => {
+      window.removeEventListener('resize', updateVideoLink);
+    };
+  }, []);
+
+  const updateVideoLink = () => {
+    let newVideoLink = window.innerWidth <= 600 ? mobileLink : pcLink;
+    // Change the source URL
+    sourceRef.current.src = newVideoLink;
+    // Load and play the new video
+    videoRef.current.load();
+    videoRef.current.play();
+  };
 
   const handleUnmute = () => {
     if (muted) {
       videoRef.current.muted = false;
+      videoRef.current.s;
       setMuted(false);
-    }else {
+    } else {
       videoRef.current.muted = true;
-      setMuted(true)
+      setMuted(true);
     }
   };
-
 
   return (
     <div className="video-headline">
@@ -28,7 +50,7 @@ const Video = () => {
         playsInline
         ref={videoRef}
       >
-        <source src="/assets/vid/headline.mp4" type="video/mp4" />
+        <source ref={sourceRef} src={pcLink} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
